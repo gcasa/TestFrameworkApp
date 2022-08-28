32a60c859e93e92692cf43c65a1b16e6ab321c20
Gregory John Casamento <greg.casamento@gmail.com>
2022-08-28 07:36:31 -0400

    Initial Commit

---
+++ TestFrameworkApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestFrameworkApp
+//
+//  Created by Gregory Casamento on 8/28/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TestFrameworkApp/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  TestFrameworkApp
+//
+//  Created by Gregory Casamento on 8/28/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
